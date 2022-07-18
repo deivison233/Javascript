eeeab6952fd9f4506a2d645590c48ce7feab57d1
deivison233 <deivisonbenitezquinto1988@gmail.com>
2022-07-18 09:26:34 -0500

    creamos una constante

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+let nombre=prompt("ingrese su nombre");
+let apellido=prompt("");
+console.log(nombre+apellido)
+
+const email="deivisonbenitezquinto1988@gmail.com"
+console.log(`el correo es :${email}`);
+
+let correo;
+console.log(`el correo es :${correo}`);
