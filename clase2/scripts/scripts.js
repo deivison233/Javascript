28d98ccdca8883075dbcf7a971ada700151d3af3
deivison233 <deivisonbenitezquinto1988@gmail.com>
2022-07-19 14:12:28 -0500

    orden de importaciones y el uso del var y let

---
+++ clase2/scripts/scripts.js
@@ -0,0 +1,13 @@
+alert("Hola mundo")
+let firstname="Deivison";
+let lastname="Benitez Quinto";
+let gender="locos";
+//let age=34;
+var age =5;
+if (gender=="male") {
+    
+}else{
+    console.log(age)
+}
+
+console.log(`Bienvenido ${firstname} ${lastname}su genero es: ${gender}y su edad es: ${age}`)
