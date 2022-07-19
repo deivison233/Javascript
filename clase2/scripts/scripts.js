ffae3294d49b98e165ff6823633352eca7b336cd
deivison233 <deivisonbenitezquinto1988@gmail.com>
2022-07-19 14:32:53 -0500

    tipos de datos

--- clase2/scripts/scripts.js
+++ clase2/scripts/scripts.js
@@ -1,13 +1,21 @@
-alert("Hola mundo")
-let firstname="Deivison";
+//alert("Hola mundo")
+{
+   let firstname="Deivison";
 let lastname="Benitez Quinto";
 let gender="locos";
-//let age=34;
-var age =5;
-if (gender=="male") {
-    
-}else{
-    console.log(age)
+let isStudent=false;
+let age=34;
+let city;
+let country=null;
+
+
+console.log(typeof country); 
 }
 
-console.log(`Bienvenido ${firstname} ${lastname}su genero es: ${gender}y su edad es: ${age}`)
+//if (gender=="male") {
+    
+//}else{
+ //   console.log(age)
+//}
+
+//console.log(`Bienvenido ${firstname} ${lastname}su genero es: ${gender}y su edad es: ${age}`)
