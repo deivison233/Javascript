09fbbe98cb7f4301ee479b80f74b9a50a9e0cfba
deivison233 <deivisonbenitezquinto1988@gmail.com>
2022-07-21 13:10:48 -0500

    esta es la clase 3 sobre arrays y objetos

---
+++ clase3/assets/app.js
@@ -0,0 +1,13 @@
+//Crear un array vacio
+let array=[]
+
+console.log(array);
+
+let numeros=[1,2,3,4]
+console.log(numeros)
+
+let animales=["perro","gato","pato","sapo"]
+console.log(animales)
+
+let colores=["verde","blanco","rojo","azul"]
+console.log(colores.length)
