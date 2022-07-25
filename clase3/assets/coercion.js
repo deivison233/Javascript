103ab093826fb0bd44a71ddd5f74f1c6e9e393eb
deivison233 <deivisonbenitezquinto1988@gmail.com>
2022-07-25 14:19:26 -0500

    conversion

---
+++ clase3/assets/coercion.js
@@ -0,0 +1,21 @@
+let num1=2,num2="3"
+
+let resulatdo=num1+num2
+
+console.log(resulatdo)
+console.log(typeof resultado)
+
+let resulatdoEntero=num1+Number(num2);
+
+console.log(resultadoEntero)
+
+let res =num1+parseInt(num2)
+
+console.log(res)
+
+let nota1=3.5,nota2="4.5"
+
+let suma=nota1parseFloat(nota2)
+
+console.log(suma)
+console.log(typeof suma.toString())
