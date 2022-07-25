df9e3ba12e2254f15f9ebd3183edd400158a58fd
deivison233 <deivisonbenitezquinto1988@gmail.com>
2022-07-25 14:30:19 -0500

    condicionales if - if else

---
+++ clase 5 js/assets/condicionales.js
@@ -0,0 +1,15 @@
+let age=19;
+//condicionales con if 
+if (age>18){
+    //console.log("eres mayor de edad");
+    document.write("eres mayor de edad")
+}
+
+//condicionales con if y else
+
+if (age>=18){
+
+    console.log("eres mayor de edad")
+}else{
+    console.log("eres menor de edad")
+}
